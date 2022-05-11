a7fd2fe3d6491c2ffbc5d13d248a7e097ad8fcd8
Axale <38008781+Axale@users.noreply.github.com>
2022-05-10 22:55:32 -0400

    5/10/22
    Start!

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,50 @@ namespace Tet3
 {
     public partial class Form1 : Form
     {
+        bool MenuMade;
+        int GameState;
         public Form1()
         {
             InitializeComponent();
+            GameState = 0;
+            MenuMade = true;
+            Menu();
+
+
         }
 
+        private void Menu()
+        {
+
+            Button Startbutton = new Button();
+            Startbutton.Size = new Size(200, 50);
+            Startbutton.Location = new Point(300, 200);
+            Startbutton.Text = "Start Game";
+
+            Button Quitbutton = new Button();
+            Quitbutton.Size = new Size(200, 50);
+            Quitbutton.Location = new Point(300, 500);
+            Quitbutton.Text = "Quit Game";
+
+            this.Controls.Add(Startbutton);
+            this.Controls.Add(Quitbutton);
+
+            Startbutton.Click += PressStart;
+            Quitbutton.Click += PressQuit;
+
+            return;
+        }
+
+        private void PressStart(object Sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            MenuMade = false;
+        }
+        
+        private void PressQuit(object Sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
+
 }
