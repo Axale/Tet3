67f58353fa53a8589e1f0066549c6703e07d8ace
Axale <38008781+Axale@users.noreply.github.com>
2022-05-17 20:21:01 -0400

    5/17/22 Changes
    Got over a big hump in the form of the clearing/cascading system, and added new blocks

--- Form1.cs
+++ Form1.cs
@@ -28,6 +28,16 @@ namespace Tet3
 
         }
 
+        private void StopDrop(object Sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Down:
+                    falltimer.Interval = 800;
+                    break;
+            }
+        }
+
         private void LatTrans(object Sender, KeyEventArgs e)
         {
             switch(e.KeyCode)
@@ -41,7 +51,15 @@ namespace Tet3
                 case Keys.Right:
                     RightPress();
                     break;
+                case Keys.Down:
+                    DropFast();
+                    break;
             }
+        }
+
+        private void DropFast()
+        {
+            falltimer.Interval = 100;
         }
 
         private void UpPress()
@@ -56,7 +74,7 @@ namespace Tet3
                 tempvects[2 * i] = CurrTet.blocks[0].Location[0] + temprel[2 * i];
                 tempvects[2 * i + 1] = CurrTet.blocks[0].Location[1] + temprel[2 * i + 1];
                 if ((tempvects[2 * i] < 0 )|| (tempvects[2 * i] > 9)) return;
-                if ((tempvects[2 * i + 1] > 19) || (tempvects[2 * i + 1] < 0)) return;
+                if ((tempvects[2 * i + 1] > 21) || (tempvects[2 * i + 1] < 0)) return;
                 if (LogicBoard.Rows[tempvects[2 * i + 1]].blockrow[tempvects[2 * i]] != null) return;
             }
 
@@ -108,7 +126,7 @@ namespace Tet3
         {
             for(int i = 0;i < 4; i++)
             {
-                if (CurrTet.blocks[i].Location[1] > 18) return true;
+                if (CurrTet.blocks[i].Location[1] > 20) return true;
                 if (LogicBoard.Rows[CurrTet.blocks[i].Location[1] + 1].blockrow[CurrTet.blocks[i].Location[0]] != null) return true;
             }
             return false;
@@ -155,6 +173,7 @@ namespace Tet3
             falltimer.Tick += TickEvent;
 
             KeyDown += LatTrans;
+            KeyUp += StopDrop;
 
             falltimer.Start();
         }
@@ -180,17 +199,20 @@ namespace Tet3
     {
         public List<Row> Rows;
         public List<int> RowsToClear;
+        int highest;
 
         public LockedBoard()
         {
-            Rows = new List<Row>(20);
+            highest = 21;
+            Rows = new List<Row>(22);
             RowsToClear = new List<int>(0);
-            for (int i = 0; i < 20; i++) Rows.Add(new Row());
+            for (int i = 0; i < 22; i++) Rows.Add(new Row());
         }
 
         public void AddBlock(Block block)
         {
             Rows[block.Location[1]].blockrow[block.Location[0]] = block;
+            if(block.Location[1] < highest) highest = block.Location[1];
             if(++Rows[block.Location[1]].count >= 10)
             {
                 RowsToClear.Add(block.Location[1]);
@@ -204,14 +226,35 @@ namespace Tet3
             int LoopRow;
             if (RowsToClear.Count == 0) return;
             LoopRow = RowsToClear.Max();
+            RowsToClear.RemoveAll(x => x == LoopRow);
 
-            while(LoopRow - offset > 0)
+            while(LoopRow >= highest)
             {
                 if (RowsToClear.Contains(LoopRow - offset)) offset++;
+                else
+                {
+                    Rows[LoopRow].ClearIm();
+                    if (LoopRow < (highest + offset))
+                    {
+                        Rows[LoopRow] = new Row();
+                    }
+                    else
+                    {
+
+                        Rows[LoopRow] = Rows[LoopRow - offset];
+                        Rows[LoopRow - offset] = new Row();
+                        Rows[LoopRow].UpdateRows(LoopRow);
+                    }
+
+                    LoopRow--;
+                }
                 
             }
+            RowsToClear.Clear();
+            return;
         }
-        public class Row
+
+        public class Row 
         {
             public List<Block?> blockrow;
             public int count;
@@ -220,29 +263,47 @@ namespace Tet3
             {
                 blockrow = new List<Block?>(10);
                 count = 0;
-                for(int i = 0; i < 10; i++)
+                for (int i = 0; i < 10; i++)
                 {
                     blockrow.Add(null);
                 }
             }
 
+            public void ClearIm()
+            {
+                for (int i = 0; i < 10; i++) if (blockrow[i] != null) blockrow[i].die();
+            }
+
+            public void UpdateRows(int LoopY)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (blockrow[i] != null)
+                    {
+                        blockrow[i].Location[1] = LoopY;
+                        blockrow[i].UpdateImage();
+                        blockrow[i].BlockTexture.Visible = true;
+                    }
+                }
+            }
         }
+
     }
 
-    public class Block
+    public class Block 
     {
         public PictureBox BlockTexture;
         public int[] Location;
         public int[] relvects;
-        public Block(int xoff, int yoff)
+        public Block(int xoff, int yoff, string color)
         {
             relvects = new int[2] {xoff, yoff};
-            Location = new int[2] { 4 + xoff, 0 + yoff };
+            Location = new int[2] { 4 + xoff, 2 + yoff };
             BlockTexture = new PictureBox();
-            BlockTexture.Location = new Point(Location[0] * 40 + 200, Location[1] * 40 + 100);
+            BlockTexture.Location = new Point(Location[0] * 40 + 200, Location[1] * 40 + 20);
             BlockTexture.Size = new Size(40, 40);
-            BlockTexture.Image = new Bitmap("..\\..\\..\\textures\\redtile.bmp");
-            if (Location[1] < 0)
+            BlockTexture.Image = new Bitmap(color);
+            if (Location[1] < 2)
             {
                 BlockTexture.Visible = false;
             }
@@ -252,14 +313,15 @@ namespace Tet3
         public void fall()
         {
             Location[1]++;
-            if (Location[1] == 0) BlockTexture.Visible = true;
             UpdateImage();
             return;
         }
 
         public void UpdateImage()
         {
-            BlockTexture.Location = new Point(200 + Location[0] * 40, 100 + Location[1] * 40);
+            BlockTexture.Location = new Point(200 + Location[0] * 40, 20 + Location[1] * 40);
+            if(Location[1] < 2) BlockTexture.Visible = false;
+            else BlockTexture.Visible = true;
         }
 
         public void left()
@@ -275,22 +337,60 @@ namespace Tet3
             UpdateImage();
             return;
         }
+
+        public void die()
+        {
+            BlockTexture.Visible = false;
+        }
+
     }
 
     public class Tetramino
     {
         public List<Block> blocks;
-        private List<string> color;
+        private string color;
         private int[] offset;
 
         public Tetramino()
         {
             this.blocks = new List<Block>(4);
-            color = new List<string>(1);
-            offset = new int[8] { 0, 0, -1, 0, 1, 0, 2, 0 };
+            Random rand = new Random();
+
+            switch (rand.Next(0, 7))
+            {
+                case (0):
+                    offset = new int[8] { 0, 0, -1, 0, 1, 0, 2, 0 };
+                    color = new string("..\\..\\..\\textures\\bluetile.bmp");
+                    break;
+                case (1):
+                    offset = new int[8] { 0, 0, 0, 1, 1, 0, 1, 1 };
+                    color = new string("..\\..\\..\\textures\\yellowtile.bmp");
+                    break;
+                case (2):
+                    offset = new int[8] { 0, 0, -1, 0, 1, 0, 0, 1 };
+                    color = new string("..\\..\\..\\textures\\purpletile.bmp");
+                    break;
+                case (3):
+                    offset = new int[8] { 0, 0, 0, 1, -1, -1, 0, -1 };
+                    color = new string("..\\..\\..\\textures\\deepbluetile.bmp");
+                    break;
+                case (4):
+                    offset = new int[8] { 0, 0, 0, 1, 1, -1, 0, -1 };
+                    color = new string("..\\..\\..\\textures\\orangetile.bmp");
+                    break;
+                case (5):
+                    offset = new int[8] { 0, 0, -1, 1, 0, 1, 1, 0 };
+                    color = new string("..\\..\\..\\textures\\greentile.bmp");
+                    break;
+                case (6):
+                    offset = new int[8] { 0, 0, 1, 1, 0, 1, -1, 0 };
+                    color = new string("..\\..\\..\\textures\\redtile.bmp");
+                    break;
+            }    
+
             for (int i = 0; i < 4; i++)
             {
-                blocks.Add(new Block(offset[2 * i], offset[2 * i + 1]));
+                blocks.Add(new Block(offset[2 * i], offset[2 * i + 1], color));
             }
         }
 
